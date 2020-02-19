ef5db2ffcb0a8444901cec8f42c5676773f0fb13
porfirovskiy <porfirovskiy@gmail.com>
2020-02-19 23:41:10 +0200

    add log out on screen

--- src/Loader.php
+++ src/Loader.php
@@ -12,6 +12,8 @@ class Loader {
     
     const LINK_CONTAINER_CLASS = 'img_filename';
     const BASE_URL = 'https://arhivach.ng/';
+    const EXTENSIONS_LIST = ['png', 'jpg', 'jpeg', 'gif'];
+    const STORAGE_DIR = 'storage';
 
     protected string $url = '';
     protected string $html = '';
@@ -27,9 +29,17 @@ class Loader {
 
     public function load(): void {
         
+        $this->printingLog("loading of images started!");
+        
+        $this->createDir($this->url);
+        
         $this->getPageContent();
         
+        $this->printingLog("html is loaded");
+        
         $urls = $this->getUrls();
+        
+        $this->printingLog("urls list is formed [" . count($urls) . " items]");
         
         $this->loadImages($urls);
         
@@ -53,18 +63,49 @@ class Loader {
         
         $dom = new \DOMDocument();
         $dom->loadHTML($this->html);
-        $tagsA = $dom->getElementsByTagName('a');
+        $tags = $dom->getElementsByTagName('a');
         
-        foreach ($tagsA as $tag) {
-            if ($tag->getAttribute('class') == self::LINK_CONTAINER_CLASS) {
-                $url = $tag->getAttribute('href');
-                $urls[$this->getImageName($url)] = $this->setCurrentUrl($url);
-            }
+        foreach ($tags as $tag) {
+            $urls = $this->getCurrentUrl($tag, $urls);
         }
         
         return $urls;
     }
     
+    /**
+     * 
+     * @param \DOMElement $tag
+     * @param array $urls
+     * @return array
+     */
+    protected function getCurrentUrl(\DOMElement $tag, array $urls): array {
+        if ($tag->getAttribute('class') == self::LINK_CONTAINER_CLASS) {
+            $url = $tag->getAttribute('href');
+            if ($this->isOnlyImageUrl($url)) {
+                $urls[$this->getImageName($url)] = $this->setCurrentUrl($url);
+           }
+        }
+        
+        return $urls;
+    }
+
+    /**
+     * 
+     * @param string $url
+     * @return bool
+     */
+    protected function isOnlyImageUrl(string $url): bool {
+        $result = pathinfo($url);
+        $extension = $result['extension'];
+        
+        if (in_array($extension, self::EXTENSIONS_LIST)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     /**
      * 
      * @param string $url
@@ -85,12 +126,17 @@ class Loader {
         return $result['path'];
     }
 
-
-    protected function createDir(string $url) {
-        
+    /**
+     * 
+     */
+    protected function createDir(): void {
+        if (!file_exists(self::STORAGE_DIR)) {
+            mkdir(self::STORAGE_DIR, 0777, true);
+            $this->printingLog("dir " . self::STORAGE_DIR . " created");
+        }
     }
     
-    protected function loadImages(array $imagesUrls) {
+    protected function loadImages(array $imagesUrls): void {
         $client = new Client(['base_uri' => self::BASE_URL]);
 
         // Initiate each request but do not block
@@ -114,9 +160,19 @@ class Loader {
         $promises = [];
         
         foreach ($imagesUrls as $key => $url) {
-            $promises[$key] = $client->getAsync($url, ['sink' => $key]);
+            $promises[$key] = $client->getAsync($url, ['sink' => self::STORAGE_DIR . "/" . $key]);
         }
         
         return $promises;
     }
+    
+    /**
+     * 
+     * @param string $message
+     * @return void
+     */
+    protected function printingLog(string $message): void {
+        echo date('Y-m-d H:m:i') . " " . $message . "\n";
+    }
+    
 }
